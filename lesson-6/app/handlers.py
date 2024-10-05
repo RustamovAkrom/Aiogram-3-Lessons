2fef60f56031a714e2008f7e08c6c9558ff769fd
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-05 22:22:11 +0500

    change errors

--- lesson-6/app/handlers.py
+++ lesson-6/app/handlers.py
@@ -1,11 +1,19 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 
 router = Router()
+
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
+
 
 
 @router.message(CommandStart())
@@ -39,3 +47,27 @@ async def get_photo(message: Message):
 async def catalog(callback: CallbackQuery):
     await callback.answer("Your selected catalog.", show_alert=True)
     await callback.message.edit_text("Salom!", reply_markup=await kb.inline_cars())
+
+
+
+
+@router.message(Command("register"))
+async def reg_one(message: Message, state: FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer("Ozizni ismingizni kiriting:")
+
+
+@router.message(Reg.name)
+async def reg_two(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer("Ozizni telefon raqamingizni kiriting:")
+
+
+@router.message(Reg.number)
+async def two_three(message: Message, state: FSMContext):
+    await state.update_data(number=message.text)
+    data = await state.get_data()
+
+    await message.answer(f"Our OK.\nISM: {data['name']}\nNUMBER: {data['number']}")
+    await state.clear()
