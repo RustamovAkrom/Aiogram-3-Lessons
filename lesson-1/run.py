95e0cf5a70f558637bc3795703e2f7b5b27648f3
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-05 21:19:37 +0500

    fix: change errors

--- lesson-1/run.py
+++ lesson-1/run.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-from .config import BOT_TOKEN
+from config import BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
