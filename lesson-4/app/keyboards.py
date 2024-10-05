e09145900dccff740bbe9262dc1b6a682f03e896
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-05 21:42:06 +0500

    end lesson-4

---
+++ lesson-4/app/keyboards.py
@@ -0,0 +1,29 @@
+from aiogram.types import (
+    ReplyKeyboardMarkup, 
+    KeyboardButton,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+
+
+main = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="Katalog")],
+    [KeyboardButton(text="Korzinga"), KeyboardButton(text="Contacts")],
+], 
+resize_keyboard=True, 
+input_field_placeholder="Select my punkt.")
+
+
+settings = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text="YouTube", url="https://youtube.com/")]
+])
+
+cars = ["Tesla", "Mercades", "MBW"]
+
+
+async def inline_cars():
+    keyboard = InlineKeyboardBuilder()
+    for car in cars:
+        keyboard.add(InlineKeyboardButton(text=car, url="https://youtube.com/"))
+    return keyboard.adjust(2).as_markup()
