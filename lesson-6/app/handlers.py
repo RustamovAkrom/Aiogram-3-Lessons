9fa0c9f64edaac4c08e2dfb8783a902e96476afa
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-05 22:18:31 +0500

    end lesson-6

---
+++ lesson-6/app/handlers.py
@@ -0,0 +1,41 @@
+from aiogram import F, Router
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+
+import app.keyboards as kb
+
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.reply(f"Salom. Sening ID raqaming: {message.from_user.id},\n{message.from_user.first_name}",
+                        reply_markup=kb.main)
+
+
+@router.message(Command("help"))
+async def get_help(message: Message):
+    await message.answer("But buyruq /help")
+
+
+@router.message(F.text == "Ishlar qanday?")
+async def how_are_you(message: Message):
+    await message.answer("OK!")
+
+
+@router.message(F.photo)
+async def get_photo(message: Message):
+    await message.answer(f"ID photo: {message.photo[-1].file_id}")
+
+
+@router.message(Command("get_photo"))
+async def get_photo(message: Message):
+    await message.answer_photo(photo="AgACAgIAAxkBAAMVZwFipPaKYKnpSEEaebRDd2mw18QAAgLlMRtb_AhIYW7xms0tVS8BAAMCAAN5AAM2BA",
+                               caption="It is your photo")
+    
+
+@router.callback_query(F.data == 'catalog')
+async def catalog(callback: CallbackQuery):
+    await callback.answer("Your selected catalog.", show_alert=True)
+    await callback.message.edit_text("Salom!", reply_markup=await kb.inline_cars())
